c06c938d059283d6aebbc08aaad21365383991a9
Mayank3891 <mayanksharmak56@gmail.com>
2026-01-12 12:42:51 +0530

    Day 5 assignment

---
+++ Day 5Assignment/ClassLibrary1/GetGreeting.cs
@@ -0,0 +1,18 @@
+﻿namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if(name == null)
+            {
+                return "hello guest";
+
+            }
+            else
+            {
+                return"hello" +name;
+            }
+        }
+    }
+}
---
+++ Day 5Assignment/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using GreetingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter your name :");
+            string a =Console.ReadLine();
+            string b= GreetingHelper.GetGreeting(a);
+            Console.WriteLine(b);
+           
+        }
+    }
+}

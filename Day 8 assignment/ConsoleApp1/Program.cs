de850415c88db2d9e23fadc70af14566685c9c88
Mayank3891 <mayanksharmak56@gmail.com>
2026-01-15 11:26:47 +0530

    Day 8 assignment

---
+++ Day 8 assignment/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SimpleUserLogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] inputArr = input.Split("|", StringSplitOptions.TrimEntries);
+
+            string userName = inputArr[0];
+            string loginMsg = inputArr[1].ToLower();
+
+            string status;
+            if (loginMsg.Contains("successful"))
+            {
+                if (loginMsg.Equals("login successful")) status = "LOGIN SUCCESS";
+                else status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+            }
+            else status = "LOGIN FAILED";
+
+            Console.WriteLine($"User {":",4} {userName}");
+            Console.WriteLine($"Message {":",1} {loginMsg}");
+            Console.WriteLine($"Status {":",2} {status}");
+        }
+    }
+}

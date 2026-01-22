bffd9e4025b34b5beea1168bd331fd1c68a6ff2a
Mayank3891 <mayanksharmak56@gmail.com>
2026-01-22 11:35:55 +0530

    Assignment

---
+++ Day 15 Assignment/Empname/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace EmployeeName
+{
+    internal class Employees
+    {
+        private int id = 1000;
+        public string Name { get; set; }
+
+        private string department;
+
+        string[] validDepartment = { "Accounts", "Sales" };
+        public string Department
+        {
+            get { return department; }
+            set { if (Array.IndexOf(validDepartment, value) > -1) department = value; }
+        }
+
+        private int salary;
+
+        public int Salary
+        {
+            get { return salary; }
+            set { if (value >= 50000 && value <= 90000) salary = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{id} {Name} {Department} {Salary}");
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees employees = new Employees();
+            employees.Name = "Mayank";
+            employees.Department = "Accounts";
+            employees.Salary = 56000;
+
+            employees.Display();
+        }
+    }
+}

d7d2c2208929ae4abcbae09df53ae7d36d0adfb7
Mayank3891 <mayanksharmak56@gmail.com>
2026-01-22 11:32:39 +0530

    Day 14

---
+++ Day 14 assignment/Print line/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PrintLine
+{
+    internal class Program
+    {
+        static void PrintLine(char sign = '*', int num = 40)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(sign);
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            PrintLine();
+            PrintLine('$');
+            PrintLine(num: 60, sign: 'a');
+        }
+    }
+}

00a16650ac1e2576cc70057a870130c3ea296163
Mayank3891 <mayanksharmak56@gmail.com>
2026-01-23 14:40:21 +0530

    Day 16

---
+++ Day 15 Assignment/exam/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static int[] Merge(int[] arr1, int[] arr2)
+    {
+        int m = arr1.Length;
+        int n = arr2.Length;
+
+        int[] arr3 = new int[m + n];
+
+        int index = 0;
+        int sum1 = 0, sum2 = 0;
+
+        // Copy arr1
+        for (int i = 0; i < m; i++)
+        {
+            arr3[index++] = arr1[i];
+            sum1 += arr1[i];
+        }
+
+        // Copy arr2
+        for (int i = 0; i < n; i++)
+        {
+            arr3[index++] = arr2[i];
+            sum2 += arr2[i];
+        }
+
+        // Reverse merged array
+        Array.Reverse(arr3);
+
+        int avg1 = sum1 / m;
+        int avg2 = sum2 / n;
+
+        Console.WriteLine("Average of arr1: " + avg1);
+        Console.WriteLine("Average of arr2: " + avg2);
+
+        return arr3;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr1 = { 71, 22, 31, 84, 58 };
+        int[] arr2 = { 16, 25, 63, 40, 85 };
+
+        int[] arr3 = Merge(arr1, arr2);
+
+        Console.WriteLine("Merged & Reversed Array:");
+        foreach (int x in arr3)
+        {
+            Console.Write(x + " ");
+        }
+    }
+}
+
---
+++ Day 16 Assignment/staticconfig/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace staticconfig
+{
+    internal class Program
+    {
+        internal class ApplicationConfig
+        {
+            static public string ApplicationName { get; set; }
+            static public string Environment {  get; set; }
+            static public int AccessCount {  get; set; }
+            static public bool IsInitialized {  get; set; }
+
+            static ApplicationConfig() {
+                ApplicationName = "MyApp";
+                Environment = "Development";
+                AccessCount = 0;
+                IsInitialized = false;
+                Console.WriteLine("static constructor executed");
+
+            }
+            static public void Initialize(string AppName, string enviornment)
+            {
+                ApplicationName = AppName;
+                Environment = enviornment;
+                IsInitialized=true;
+                AccessCount++;
+
+            }
+            static public string GetConfigurationSummary()
+            {
+                AccessCount++;
+                return $"{ApplicationName}  {Environment}   {AccessCount}   {IsInitialized}";
+            }
+            static public void ResetConfiguration()
+            {
+                ApplicationName = "MyApp";
+                Environment = "Development";
+                IsInitialized = false;
+                AccessCount++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            ApplicationConfig.Initialize("Another app","WEBSITE");
+            Console.WriteLine(ApplicationConfig.GetConfigurationSummary());
+            ApplicationConfig.ResetConfiguration();
+            Console.WriteLine(ApplicationConfig.GetConfigurationSummary());
+        }
+    }
+}

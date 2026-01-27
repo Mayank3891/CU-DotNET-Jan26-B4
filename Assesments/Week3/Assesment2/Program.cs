0f826a75e3fe4a2fe5e5d952b213b39a60ae1bf2
Mayank3891 <mayanksharmak56@gmail.com>
2026-01-27 14:09:15 +0530

    Day 18 Assignment

---
+++ Assesments/Week3/Assesment2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Assesment2
+{
+    internal class Program
+    {
+        //static bool Checkpali(string t)
+        //{
+        //     int j=t.Length-1;
+        //    int sum = 0;
+        //    int p = t.Length / 2;
+        //    for(int i=0;i<p; i++)
+        //    {
+               
+        //        if(t[i] == t[j])
+        //        {
+        //            sum++;
+        //        }
+        //        j--;
+        //    }
+        //    if (sum == p)
+        //    {
+        //        return true;
+        //    }
+        //    return false;
+        //}
+        static void Main(string[] args) {
+            //{
+            //    string t =  "abcdcba" ;
+            //    bool m = Checkpali(t);
+            //    Console.WriteLine(m);
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int k = 1; k <= Math.Min(i,10-1);k++)
+            //    {
+            //        Console.Write(k);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //    //for (int k = 1; k<= i; k++)
+            //    //{
+
+            //    //  Console.Write(k);
+
+            //    //}
+            int m = 5;
+            for (int i = 0; i <m; i++)
+            {
+                for(int j = 0; j <m; j++)
+                {
+                    if (i == j || (i + j) == m-1 ) 
+                    {
+                        Console.Write("1");
+                    }
+                    
+                  else
+                        Console.Write("0");
+                }
+                Console.WriteLine();
+            }
+
+             
+        }
+    }
+}
---
+++ Day 18 assignment/Loaninherit/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Loaninherit
+{
+    internal class Program
+    {
+        public class Loan
+        {
+             public string LoanNumber {  get; set; }
+            public string CustomerName {  get; set; }
+            public decimal PrincipalAmount {  get; set; }
+            public int TenureInYears { get; set; }
+
+             public Loan()
+            {
+                LoanNumber = "home";
+                CustomerName = "Shivam";
+                PrincipalAmount = 35000;
+                TenureInYears = 5;
+                    
+            }
+            public decimal CalculateEmi()
+            {
+                return (decimal)((PrincipalAmount + (PrincipalAmount / 10))/TenureInYears);
+            }
+        }
+        class Homeloan : Loan
+        {
+            public decimal CalculateEmi()
+            {
+                return (decimal)((PrincipalAmount + (PrincipalAmount / 11)) / TenureInYears);
+            }
+        }
+
+        class Carloan : Loan
+        {
+            public decimal CalculateEmi()
+            {
+                return (decimal)((PrincipalAmount + (PrincipalAmount / 12)) / TenureInYears);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Loan[] Loans = new Loan[]
+            {
+                new Carloan(),
+                new Homeloan(),
+                new Carloan(),
+                new Homeloan(),
+            };
+            for (int i = 0; i < Loans.Length; i++)
+            {
+                Console.WriteLine(Loans[i].CalculateEmi());
+            }
+
+
+        }
+    }
+}

a5fcccbbf15cb5dc1f166e929e09e3a45f44bc02
Mayank3891 <mayanksharmak56@gmail.com>
2026-02-03 14:58:20 +0530

    Assignment

---
+++ Day 24/Leadersboard/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Leadersboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double,string> lead = new SortedDictionary<double,string>();
+
+            lead.Add(55.42, "SwiftRacer");
+            lead.Add(52.10, "SpeedDemon");
+            lead.Add(58.91, "SteadyEddie");
+            lead.Add(51.05, "TurboTom");
+
+            foreach (var k in lead) {
+                Console.WriteLine($"Time is   :{k.Key} and name is  :{k.Value}");
+            }
+            var fastest = lead.First();
+            Console.WriteLine("");
+            Console.WriteLine($"Fastest racer is :{fastest}");
+            double r = 0;
+            string st = "SteadyEddie";
+            foreach (var h in lead)
+            {
+                if (st == h.Value) 
+                {
+                    r = h.Key;
+                }
+            }
+            lead.Remove(r);
+            lead.Add(54.00, "SteadyEddie");
+            Console.WriteLine("");
+            foreach (var k in lead)
+            {
+                Console.WriteLine($"Time is   :{k.Key} and name is  :{k.Value}");
+            }
+
+        }
+    }
+}
---
+++ Day 24/hash employee/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace hash_employee
+{
+    internal class Program
+    {
+        public  static void details( Hashtable e,int id, string name)
+        {
+            if (e.ContainsKey(id))
+            {
+                Console.WriteLine("ID already exists");
+            }
+            else { 
+                e.Add(id, name);
+                Console.WriteLine("Employee details added");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Hashtable emp = new Hashtable();
+            details(emp, 101, "Alice");
+
+            details(emp, 102, "Bob");
+            details(emp, 103, "Charlie");
+            details(emp, 104, "Diana");
+
+
+
+            Object emp1 = emp[102];
+            Console.WriteLine("");
+            string p = (string)emp1;
+            Console.WriteLine($"Name of employee is {p}");
+            Console.WriteLine("");
+            foreach(DictionaryEntry entry in emp)
+            {
+                Console.WriteLine($"ID:{entry.Key} Name: {entry.Value}");
+            }
+
+            emp.Remove(103);
+            Console.WriteLine("");
+            Console.WriteLine($"Total employee are:{emp.Count}");
+        }
+    }
+}

3d097e02cb350d33a4aaa9cda5ed6b7cc1ba7f8b
Mayank3891 <mayanksharmak56@gmail.com>
2026-02-09 15:58:28 +0530

    Assignment

---
+++ Day 27/Loanportfolio/Program.cs
@@ -0,0 +1,122 @@
+﻿
+using System.Runtime.InteropServices.Marshalling;
+using System.IO;
+
+namespace Loanportfolio
+{
+    class RestrictedDestinationexception : Exception
+    {
+        public RestrictedDestinationexception(string destination) { 
+
+            Console.WriteLine($"your destination is restricted{destination}");
+        }
+    }
+    class Insecurepackagingexception : Exception
+    {
+        public Insecurepackagingexception()
+        {
+            Console.WriteLine("your package is insecure to ship");
+        }
+    }
+    abstract class Shipment
+    {
+        public string TrackingId {  get; set; }
+        public string Destination {  get; set; }
+        public double weight {  get; set; }
+        public bool isFragile {  get; set; }
+        public bool isReinforced {  get; set; }
+
+
+        public Shipment(string tracking
+
+        public virtual void Processing() { }
+    }
+  
+    class Expressshipment : Shipment
+    {
+        public Expressshipment(string trackingId, double weight,string destination,bool fragile,bool rein) : base(TrackingId, weight, destination, isFragile, isReinforced)
+        {
+
+        }
+        public override void Processing()
+        {
+            HashSet<string> ht = new HashSet<string>()
+            {
+               "north pole","unknown island"
+            };
+            if (weight <=0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (ht.Contains(Destination))
+            {
+                throw new RestrictedDestinationexception(Destination);
+            }
+            if(isFragile && !isReinforced)
+            {
+                throw new Insecurepackagingexception();
+            }
+
+        }
+    }
+    class HeavyFreight:Shipment
+    {
+        public override void Processing()
+        {
+            bool isPermit = true;
+            HashSet<string> ht = new HashSet<string>()
+            {
+               "north pole","unknown island"
+            };
+            if (weight <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (ht.Contains(Destination))
+            {
+                throw new RestrictedDestinationexception(Destination);
+            }
+            if (isFragile && !isReinforced)
+            {
+                throw new Insecurepackagingexception();
+            }
+            if(weight>=1000&& !isPermit)
+            {
+                throw new Exception("Permit required");
+            }
+
+        }
+    }
+    interface Iloggable
+    {
+        public void savelog(string message);
+    }
+    class Logmanager : Iloggable
+    {
+        string path = "../../../shipment_audit.log";
+        
+        public void savelog(string message)
+        {
+            
+                using StreamWriter writer = new StreamWriter(path, true);
+                writer.WriteLine(message);
+            
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Iloggable loggable = new Logmanager();
+            loggable.savelog("==================Application started====================");
+
+            List<Shipment> shipments = new List<Shipment>()
+            {
+                new Expressshipment(TrackingId="A1",Destination="north pole",weight=654,isFragile=true,isReinforced=true),
+
+
+            };
+        }
+    }
+}
---
+++ Day 29/Kitchenappliance/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace Kitchenappliance
+{
+    interface Itimer
+    {
+        void Settimer(int minutes);
+    }
+    interface Iwifi
+    {
+        void connectwifi();
+    }
+    abstract class KitchenDevice
+    {
+        public string Modelname { get; set; }
+        public int Powerconsumption { get; set; }
+
+        public KitchenDevice(string modelname, int powerconsumption)
+        {
+            Modelname = modelname;
+            Powerconsumption = powerconsumption;
+        }
+        public abstract void cook();
+        public virtual void Preheat()
+        {
+            Console.WriteLine("Appliance require   Preheat");
+        }
+    }
+
+    class Microwave : KitchenDevice,Itimer
+    {
+        public Microwave(string modelname, int powerconsumption) : base(modelname, powerconsumption)
+        {
+        }
+        public override void cook()
+        {
+            Console.WriteLine($"Microwave: {Modelname} IS cooking food");
+        }
+        public void Settimer(int minutes)
+        {
+            Console.WriteLine($"Microwave is set for {minutes}");
+        }
+    }
+
+    class Oven : KitchenDevice, Itimer, Iwifi
+    {
+        public Oven(string modelname, int powerconsumption) : base(modelname, powerconsumption)
+        {
+        }
+      
+        public override void cook()
+        {
+            Console.WriteLine($"Microwave: {Modelname} IS cooking food");
+        }
+        public void Settimer(int minutes)
+        {
+            Console.WriteLine($"Microwave is set for {minutes}");
+        }
+        public void connectwifi()
+        {
+            Console.WriteLine("Oven is being connected to wifi");
+        }
+    }
+    class Airfryer : KitchenDevice
+    {
+        public Airfryer(string modelname, int powerconsumption) : base(modelname, powerconsumption)
+        {
+        }
+        public override void cook()
+        {
+            Console.WriteLine("airfryer is cookin food");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<KitchenDevice> kd = new List<KitchenDevice>()
+            {
+                new Microwave("abc",9876),
+                new Oven("cgd",9876),
+                new Airfryer("uv",9876)
+            };
+
+            foreach(var appliance  in kd)
+            {
+                appliance.cook();
+                appliance.Preheat();
+                if(appliance is Itimer timerapp)
+                {
+                    timerapp.Settimer(78);
+                }
+                if(appliance is Iwifi wiapp)
+                {
+                    wiapp.connectwifi();
+                }
+            }
+
+            
+
+        }
+    }
+}

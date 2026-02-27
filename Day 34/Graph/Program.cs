a9d3c30286beedbae1d6902cfb1dfe6d1672e12c
Mayank3891 <mayanksharmak56@gmail.com>
2026-02-27 09:30:24 +0530

    Assignment

---
+++ Day 34/Graph/Program.cs
@@ -0,0 +1,106 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+namespace TheSocialNetwork
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        List<Person> friends = new List<Person>();
+
+        public void AddFriend(Person p)
+        {
+            if (p.Id == this.Id || this.friends.Contains(p)) return;
+            friends.Add(p);
+            p.friends.Add(this);
+        }
+        public string ShowFriends()
+        {
+            string a = string.Empty;
+
+            var myfriend = friends.Select(x => new string(x.Name));
+            a = string.Join(", ", myfriend);
+            return a + "\n";
+        }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+    }
+
+    class SocialNetwork
+    {
+        List<Person> members = new List<Person>();
+
+        public void AddMember(Person p)
+        {
+            members.Add(p);
+        }
+
+        public string ShowNetwork()
+        {
+            string a = "";
+            foreach (Person p in members)
+            {
+                a += $"{p.Name}'s friends are :-\n";
+                a += p.ShowFriends() == "\n" ? $"{p.Name} is a lonely boy\n" : p.ShowFriends();
+            }
+
+            return a;
+        }
+
+        public bool AddFriend(Person p1, Person p2, out Person? p3)
+        {
+            if (!members.Contains(p1) || !members.Contains(p2))
+            {
+                p3 = members.Contains(p1) ? p2 : p1;
+                return false;
+            }
+            p1.AddFriend(p2);
+            p3 = null;
+            return true;
+        }
+    }
+
+
+    internal class GraphSocialNetwork
+    {
+        static void Main(string[] args)
+        {
+            Person aaroh = new Person(1, "Aaroh");
+            Person hritik = new Person(2, "Hritik");
+            Person sahil = new Person(3, "Sahil");
+            Person tushar = new Person(4, "Tushar");
+            Person mayank = new Person(5, "Mayank");
+
+            SocialNetwork socialNetwork = new SocialNetwork();
+
+            socialNetwork.AddMember(tushar);
+            socialNetwork.AddMember(hritik);
+            socialNetwork.AddMember(sahil);
+            socialNetwork.AddMember(mayank);
+
+            Person? p3;
+
+            if (!socialNetwork.AddFriend(sahil, tushar, out p3))
+                Console.WriteLine($"{p3.Name} does not exist in the network");
+
+            if (!socialNetwork.AddFriend(tushar, hritik, out p3))
+                Console.WriteLine($"{p3.Name} does not exist in the network");
+
+            if (!socialNetwork.AddFriend(tushar, sahil, out p3))
+                Console.WriteLine($"{p3.Name} does not exist in the network");
+
+            if (!socialNetwork.AddFriend(aaroh, sahil, out p3))
+                Console.WriteLine($"{p3.Name} does not exist in the network");
+
+            Console.WriteLine(socialNetwork.ShowNetwork());
+
+        }
+    }
+}
---
+++ Day 35/Treeorg/Program.cs
@@ -0,0 +1,81 @@
+﻿
+public class EmployeeNode
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+    public List<EmployeeNode> Reports { get; set; } = new List<EmployeeNode>();
+
+    public EmployeeNode(string name, string position)
+    {
+        Name = name;
+        Position = position;
+    }
+    public void AddReport(EmployeeNode employee)
+    {
+        Reports.Add(employee);
+    }
+}
+
+public class OrganizationTree
+{
+    public EmployeeNode Root { get; private set; }
+
+    public OrganizationTree(EmployeeNode rootEmployee)
+    {
+        Root = rootEmployee;
+    }
+
+    public void DisplayFullHierarchy()
+    {
+        PrintRecursive(Root, 0);
+    }
+
+    private void PrintRecursive(EmployeeNode current, int depth)
+    {
+        if (current == null)
+        {
+            return;
+        }
+        for (int i = 0; i < depth; i++)
+        {
+            Console.Write("   ");
+        }
+
+        Console.WriteLine($"->{current.Name}-{current.Position}");
+        foreach (var report in current.Reports)
+        {
+            PrintRecursive(report, depth+1);
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        var ceo = new EmployeeNode("Aman", "CEO");
+        var cto = new EmployeeNode("Suresh", "CTO");
+        var manager = new EmployeeNode("Sonia", "Dev Manager");
+        var dev1 = new EmployeeNode("Sara", "Senior Dev");
+        var dev2 = new EmployeeNode("Divakar", "Junior Dev");
+        var cfo = new EmployeeNode("Rajesh", "CFO");
+        var acccOfficer = new EmployeeNode("Rajat", "Account Officer");
+
+        
+        var company = new OrganizationTree(ceo);
+
+        ceo.AddReport(cto);
+
+        cto.AddReport(manager);
+        manager.AddReport(dev1);
+        manager.AddReport(dev2);
+
+        ceo.AddReport(cfo);
+        cfo.AddReport(acccOfficer);
+
+        company.DisplayFullHierarchy();
+    }
+}
+

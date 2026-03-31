665b4c68792509ca2d273c12ee583f1a83300e8a
Mayank3891 <mayanksharmak56@gmail.com>
2026-03-31 14:48:25 +0530

    Week 1 assignment

---
+++ Assesments/Week 1/Greetinglib/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (String.IsNullOrEmpty(name)) return "Hello Guest";
+            else return "Hello " + name;
+        }
+    }
+}
---
+++ Assesments/Week 1/Greetingsol/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name = ");
+            string name = Console.ReadLine();
+            Console.WriteLine(GreetingHelper.GetGreeting(name));
+        }
+    }
+}
